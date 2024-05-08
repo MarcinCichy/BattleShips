8bff5f18ef16251662b3add5ab1264712a64a94b
MarcinCichy <marcin-cichy@wp.pl>
2024-05-08 23:15:58 +0200

    Blueprint for Battleships C/S

---
+++ server_data.py
@@ -0,0 +1,26 @@
+from datetime import datetime
+
+"""
+    Data for the Server part
+"""
+
+# ----------------------------------------------------
+
+HOST = "127.0.0.1"
+PORT = 65432
+BUFFER_SIZE = 1024
+ENCODE_FORMAT = "utf-8"
+# USERS_DATABASE = "users.json"
+# MESSAGES_DATABASE = "messages.json"
+
+# ----------------------------------------------------
+
+CLOSE = "close"
+
+# ----------------------------------------------------
+
+# START_TIME = datetime.now()
+# DATE = datetime.now().strftime("%Y-%m-%d")
+# VERSION = "0.1.8"
+
+# ----------------------------------------------------
---
+++ utils.py
@@ -0,0 +1,16 @@
+from os import name, system
+
+
+class SystemUtilities:
+    def __init__(self):
+        pass
+
+    @staticmethod
+    def clear_screen():
+        """Clear the screen in depends on operating system
+        (Windows, Linux or iOS)."""
+
+        if name == "nt":
+            _ = system("cls")
+        else:
+            _ = system("clear")
