6cb1c468739039625f62d645625ab31e8c91a964
MarcinCichy <marcin-cichy@wp.pl>
2024-05-31 23:15:45 +0200

    Further preparation to use correct commands. Create blueprint of commands.

---
+++ client_commands.py
@@ -0,0 +1,27 @@
+GAME_INVITATION = {
+  "type": "GAME_INVITATION",
+  "body": None
+}
+
+SHOT = {
+  "type":"SHOT",
+  "body":{
+    "row": None,
+    "column": None
+  }
+}
+
+SHOT_REQUEST = {
+  "type": "SHOT_REQUEST",
+  "body": None
+}
+
+BOARD = {
+  "type": "BOARD",
+  "body": {
+    "four": None,
+    "three": None,
+    "two": None,
+    "one": None
+    }
+}
